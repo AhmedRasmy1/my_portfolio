b59986202a28cdcf7ca81a4bf76600662654bccd
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-09-20 19:09:15 +0300

    Refactor HomePage to conditionally render headers based on screen size and add size constants

---
+++ lib/constants/size.dart
@@ -0,0 +1,4 @@
+const double smallScreenWidth = 600;
+const double mediumScreenWidth = 800;
+const double largeScreenWidth = 1200;
+const double extraLargeScreenWidth = 1700;
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,10 +2,10 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/constants/colors.dart';
-import 'package:my_portfolio/constants/nav_items.dart';
+import 'package:my_portfolio/constants/size.dart';
 import 'package:my_portfolio/widgets/drawer_mobil.dart';
 import 'package:my_portfolio/widgets/header_for_mobil.dart';
-import 'package:my_portfolio/widgets/site_logo.dart';
+import 'package:my_portfolio/widgets/header_for_website.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,30 +19,40 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: CustomColor.scaffoldBg,
-      endDrawer: DrawerMobile(),
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          Builder(
-            builder: (context) {
-              return HeaderForMobile(
-                onLogoTap: () {},
-                onMenuTap: () {
-                  Scaffold.of(context).openEndDrawer();
-                  log("Menu Tapped");
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: CustomColor.scaffoldBg,
+          endDrawer: constraints.maxWidth > smallScreenWidth
+              ? null
+              : DrawerMobile(),
+          body: ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              Builder(
+                builder: (context) {
+                  if (constraints.maxWidth > smallScreenWidth) {
+                    return HeaderForWebsite();
+                  } else {
+                    return HeaderForMobile(
+                      onLogoTap: () {},
+                      onMenuTap: () {
+                        Scaffold.of(context).openEndDrawer();
+                        log("Menu Tapped");
+                      },
+                    );
+                  }
                 },
-              );
-            },
+              ),
+              Container(height: 500, color: Colors.blueGrey),
+              Container(height: 500, color: Colors.blueGrey),
+              Container(height: 500, color: Colors.blueGrey),
+              Container(height: 100, color: Colors.blueGrey),
+            ],
           ),
-          Container(height: 500, color: Colors.blueGrey),
-          Container(height: 500, color: Colors.blueGrey),
-          Container(height: 500, color: Colors.blueGrey),
-          Container(height: 100, color: Colors.blueGrey),
-        ],
-      ),
+        );
+      },
     );
   }
 }

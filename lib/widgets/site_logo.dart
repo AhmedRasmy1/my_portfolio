d0575fdb77cded39b6f1bb5ae3cbb83ef20c4c07
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2025-09-21 22:58:08 +0300

    Replace text logo with image asset in SiteLogo widget

--- lib/widgets/site_logo.dart
+++ lib/widgets/site_logo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_portfolio/constants/colors.dart';
 
 class SiteLogo extends StatelessWidget {
   const SiteLogo({super.key, this.onTap});
@@ -8,14 +7,10 @@ class SiteLogo extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Text(
-        "AHR",
-        style: TextStyle(
-          fontSize: 20,
-          color: CustomColor.yellowPrimary,
-          fontWeight: FontWeight.bold,
-          decoration: TextDecoration.underline,
-        ),
+      child: Image.asset(
+        'assets/images/logo.png',
+        height: 40,
+        fit: BoxFit.contain,
       ),
     );
   }
